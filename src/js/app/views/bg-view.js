dc29155e828153825c398e0042b318a39ac0a86c
CJ Gammon <gammon@adobe.com>
2015-09-30 11:08:54 -0500

    embedding 3d fly around into presentation

--- src/js/app/views/bg-view.js
+++ src/js/app/views/bg-view.js
@@ -1,6 +1,6 @@
 /*global define THREE $ TweenMax*/
 define(function (require) {
-	
+
 	var Backbone = require('backbone'),
 		Vars = require('pres/models/vars'),
 		UserEvent = require('pres/events/user-event'),
@@ -14,47 +14,98 @@ define(function (require) {
 		ZigBg = require('app/views/bg/trans-zig-bg'),
 		ColorSquaresBg = require('app/views/bg/trans-squares-color-bg'),
 		SquaresBg = require('app/views/bg/trans-squares-bg'),
+		TerrainBg = require('app/views/bg/terrain-bg'),
 		BG_ARRAY = [CutBg, CircleBg, ArcBg, RectBg, StripeBg, StripeZig, ZigBg, SquaresBg, ColorSquaresBg],
 		currentSlide,
+		isTerrain,
 		currentBg,
 		BgView;
-	
+
 	BgView = Backbone.View.extend({
 
 		initialize: function () {
-				
+
 			this.$el = $('#bg');
 			this.canvas = this.$el[0];
+			this.$webglEl = $('#bg-3d');
+			this.webglCanvas = this.$webglEl[0];
 			this.ctx = this.canvas.getContext('2d');
+
+			this.addRenderer();
+
 			this.resize();
 			UserEvent.on('resize', this.resize, this);
 		},
-		
+
+		addRenderer: function () {
+			this.renderer = new THREE.WebGLRenderer({canvas: this.webglCanvas, antialias: true});
+			this.renderer.setSize(window.innerWidth, window.innerHeight);
+			this.renderer.setClearColor(0x000000);
+		},
+
 		render: function () {
 			var currentView,
 				slide;
-			
+
 			if (currentSlide !== Vars.get('currentSlide')) {
 				slide = Vars.get('slides').at(Vars.get('currentSlide'));
-				
-				this.handleNormalSlide();
+
+				if (typeof(slide.get('view').$el.data('pos')) !== 'undefined') {
+					this.handleTerrainSlide();
+				} else {
+					this.handleNormalSlide();
+				}
 			}
-		
+
 			if (currentBg) {
 				currentBg.render();
 			}
 		},
-		
+
+		handleTerrainSlide: function () {
+			if (!isTerrain) {
+				if (typeof(currentSlide) !== 'undefined' && currentBg) {
+					currentBg.destroy();
+				}
+
+				this.$el.hide();
+				this.$webglEl.show();
+
+				currentSlide = Vars.get('currentSlide');
+
+				currentBg = null;
+				currentBg = new TerrainBg();
+				currentBg.init(this.renderer);
+				isTerrain = true;
+
+				this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+			}
+
+			return;
+		},
+
 		handleNormalSlide: function () {
+
 			var slide = Vars.get('slides').at(Vars.get('currentSlide'));
-			
+
 			if (typeof(currentSlide) !== 'undefined' && currentBg) {
 				currentBg.destroy();
 			}
-			
+
+			this.$el.show();
+
+			//allow transition
+			if (isTerrain) {
+				setTimeout(function () {
+					this.$webglEl.hide();
+				}, 1000);
+			} else {
+				this.$webglEl.hide();
+			}
+
 			currentSlide = Vars.get('currentSlide');
 			currentView = this.getCurrentView(slide);
-			
+
 			if (currentView) {
 				currentBg = null;
 				currentBg = new currentView();
@@ -63,12 +114,13 @@ define(function (require) {
 				currentBg = null;
 				this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 			}
-			
+
+			isTerrain = false;
 		},
 
 		getCurrentView: function (slide) {
 			var transition = slide.get('view').$el.data('trans');
-					
+
 			if (transition) {
 				return BG_ARRAY[parseInt(transition)];
 			} else if (slide.get('view').$el.data('color')) {
@@ -82,6 +134,6 @@ define(function (require) {
 			this.render();
 		}
 	});
-		
+
 	return BgView;
 });
